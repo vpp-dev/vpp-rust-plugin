d600da0dfeb9dacbc516d201e3e8eaabdd935b44
Andrew Yourtchenko <ayourtch@gmail.com>
2016-09-25 18:43:29 +0000

    Cleanups/some explanations.

--- src/rust_plugin.rs
+++ src/rust_plugin.rs
@@ -1,18 +1,15 @@
 use std::ptr;
 
-
 #[repr(C)]
 pub struct clib_error_t;
 #[repr(C)]
 pub struct unformat_input_t;
 
 #[repr(C)]
-pub struct vlib_cli_command_function_t; // FIXME
-
-#[repr(C)]
 pub struct vlib_init_function_list_elt {
   next_init_function: *mut vlib_init_function_list_elt,
   f: extern "C" fn(*mut vlib_main_t) -> *const clib_error_t,
+  /* FIXME: Without the padding there are strange crashes. Need to understand better what is going on */
   padding: [u8; 64],
 }
 
@@ -25,6 +22,7 @@ pub struct vlib_cli_command_t {
   short_help: *const u8,
   long_help: *const u8,
   function: extern "C" fn(*mut vlib_main_t, *mut unformat_input_t, *mut vlib_cli_command_t) -> *const clib_error_t,
+  /* There are unused fields, but nonetheless some padding is needed for the same reasons as in function_list_elt structure */
   padding: [u8; 64],
 } 
 
@@ -44,7 +42,7 @@ pub extern "C" fn aytest() {
 
 #[no_mangle]
 pub extern "C" fn rust_plugin_init(vm: *mut vlib_main_t) -> *const clib_error_t {
-  let path = b"rust test\0"; // \0\0\0\0\0\0\0";
+  let path = b"rust test\0"; // \0\0\0\0\0\0\0"; // this was needed to prevent crashing while the padding was not in place.
   let short_help = b"short help\0"; // \0\0\0\0\0\0\0";
   let long_help = b"rust long help\0";
   let foo = b"asd\0".as_ptr();
@@ -75,6 +73,10 @@ pub extern "C" fn vlib_plugin_register(vm: *mut vlib_main_t, h: *const i32, from
 }
 
 /*
+
+This contains the magic incantations to make the code executed at library load time.
+Does happen even before vlib_plugin_register, so probably is not useful.
+
 #[no_mangle]
 fn rust_plugin_init() {
     println!("I was called by .init");
