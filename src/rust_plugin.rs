e68f94630caf7c6c535789911b2ea05f2d3f00c2
Andrew Yourtchenko <ayourtch@gmail.com>
2016-09-26 16:27:27 +0000

    Padding was just a cascading error due to a mismatch in size of one of the structs.

--- src/rust_plugin.rs
+++ src/rust_plugin.rs
@@ -2,28 +2,42 @@ use std::ptr;
 
 #[repr(C)]
 pub struct clib_error_t;
+
 #[repr(C)]
 pub struct unformat_input_t;
 
 #[repr(C)]
+pub struct vnet_main_t;
+
+#[repr(C)]
+pub struct ethernet_main_t;
+
+#[repr(C)]
+pub struct vnet_plugin_handoff_t {
+  vnet_main: *const vnet_main_t,
+  ethernet_main: *const ethernet_main_t,
+}
+
+#[repr(C, packed)]
 pub struct vlib_init_function_list_elt {
   next_init_function: *mut vlib_init_function_list_elt,
   f: extern "C" fn(*mut vlib_main_t) -> *const clib_error_t,
-  /* FIXME: Without the padding there are strange crashes. Need to understand better what is going on */
-  padding: [u8; 64],
 }
 
 #[repr(C)]
 pub struct vlib_main_t;
 
+
+/* padding is all the unused fields. 96 is the sizeof in C, and 4*4 pointers */
+const vlib_cli_command_t_padding_sz: usize = 96 -4 -4 -4 -4;
 #[repr(C)]
 pub struct vlib_cli_command_t {
   path: *const u8,
   short_help: *const u8,
   long_help: *const u8,
   function: extern "C" fn(*mut vlib_main_t, *mut unformat_input_t, *mut vlib_cli_command_t) -> *const clib_error_t,
-  /* There are unused fields, but nonetheless some padding is needed for the same reasons as in function_list_elt structure */
-  padding: [u8; 64],
+  /* unused fields */
+  padding: [u8; vlib_cli_command_t_padding_sz]
 } 
 
 extern "C" {
@@ -42,8 +56,8 @@ pub extern "C" fn aytest() {
 
 #[no_mangle]
 pub extern "C" fn rust_plugin_init(vm: *mut vlib_main_t) -> *const clib_error_t {
-  let path = b"rust test\0"; // \0\0\0\0\0\0\0"; // this was needed to prevent crashing while the padding was not in place.
-  let short_help = b"short help\0"; // \0\0\0\0\0\0\0";
+  let path = b"rust test\0";
+  let short_help = b"short help\0";
   let long_help = b"rust long help\0";
   let foo = b"asd\0".as_ptr();
 
@@ -52,7 +66,7 @@ pub extern "C" fn rust_plugin_init(vm: *mut vlib_main_t) -> *const clib_error_t 
     short_help: short_help.as_ptr(),
     long_help: long_help.as_ptr(),
     function: my_cli,
-    padding: [0; 64],
+    padding: [0; vlib_cli_command_t_padding_sz]
   };
 
   unsafe {
@@ -66,7 +80,12 @@ pub extern "C" fn rust_plugin_init(vm: *mut vlib_main_t) -> *const clib_error_t 
 }
 
 #[no_mangle]
-pub extern "C" fn vlib_plugin_register(vm: *mut vlib_main_t, h: *const i32, from_early_init: i32) -> *const clib_error_t {
+pub extern "C" fn vlib_plugin_register(vm: *mut vlib_main_t, h: *const vnet_plugin_handoff_t, from_early_init: i32) -> *const clib_error_t {
+  unsafe {
+    let vnm = (*h).vnet_main;
+    let em = (*h).ethernet_main;
+    println!("vnm: {} em: {}", vnm as usize, em as usize);
+  }
 
   rust_plugin_init(vm);
   return ptr::null();
