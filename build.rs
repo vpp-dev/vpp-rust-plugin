59a3e9e31e3f3ba980dd6e05288dbbd53e639158
Andrew Yourtchenko <ayourtch@gmail.com>
2018-02-07 21:58:09 +0000

    do not try to link with libz, and assume vpp build tree is alongside

--- build.rs
+++ build.rs
@@ -8,7 +8,7 @@ use bindgen::builder;
 fn main() {
     // Tell cargo to tell rustc to link the system bzip2
     // shared library.
-    println!("cargo:rustc-link-lib=bz2");
+    //println!("cargo:rustc-link-lib=bz2");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -18,6 +18,7 @@ fn main() {
         // The input header we would like to generate
         // bindings for.
         .header("wrapper.h")
+        .clang_arg("-I../vpp/build-root/install-vpp_debug-native/vpp/include")
         .no_convert_floats()
         .hide_type("max_align_t")
         .opaque_type("vnet_main_t")
