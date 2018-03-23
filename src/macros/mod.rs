1a75fea4c85aaef6557106547a107655368e4d26
Andrew Yourtchenko <ayourtch@gmail.com>
2018-03-23 20:48:46 +0100

    add macros for representing C unsigned char strings

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -13,4 +13,16 @@ macro_rules! cstr_mut {
   );
 }
 
+macro_rules! ucstr {
+  ($s:expr) => (
+    concat!($s, "\0") as *const str as *const [u8] as *const u8
+  );
+}
+
+macro_rules! ucstr_mut {
+  ($s:expr) => (
+    concat!($s, "\0") as *const str as *mut [u8] as *mut u8
+  );
+}
+
 
